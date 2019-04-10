664a9cdec70ded1e46843d5bfe12671f41557f3f
Z-S-Burke <zach@zsburke.com>
2019-04-10 16:10:33 +0200

    added captions to the home page carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,34 +17,41 @@
       <a class="text-light" href="https://www.twitch.tv/overwatchleague">
         <b-carousel-slide
           img-src="https://techraptor.net/wp-content/uploads/2018/01/overwatch-league-twitch-902x507.jpg"
-        ></b-carousel-slide>
+        >
+        </b-carousel-slide>
       </a>
 
       <router-link class="text-light" to="/teams">
         <b-carousel-slide
           img-src="https://d2y4mhrku00tr3.cloudfront.net/images/pages/about/team-logos-40f002676ef0d61a27cee08d85358575bee03e5b2374f52d2fa1b2b7fb0f061ada398c1c99e32189c64d9628b21b091f939c84ed6066ad179193de6a6305e004.jpg"
-        ></b-carousel-slide>
+        >
+          <caption class="text-light caption_background px-4">
+            <h2>TEAMS</h2>
+          </caption>
+        </b-carousel-slide>
       </router-link>
 
-      <router-link class="text-light" to="/players">
+      <router-link to="/players">
         <b-carousel-slide
           img-src="https://i.kinja-img.com/gawker-media/image/upload/s--Fic8xpVD--/c_scale,f_auto,fl_progressive,q_80,w_800/mmadas61b3ke1xgzcdsh.jpg"
-        ></b-carousel-slide>
+        >
+          <caption class="text-light caption_background px-4 d-flex justify-content-center">
+            <h2>PLAYERS</h2>
+          </caption>
+        </b-carousel-slide>
       </router-link>
 
-      <router-link class="text-light" to="/schedule">
+      <router-link to="/schedule">
         <b-carousel-slide
           img-src="https://cdn3.dualshockers.com/wp-content/uploads/2018/07/Overwatch-LEague-ds1-1340x1340.jpg"
-        ></b-carousel-slide>
+        >
+          <caption class="text-light caption_background d-flex justify-content-end">
+            <h2>SCHEDULE</h2>
+          </caption>
+        </b-carousel-slide>
       </router-link>
-      
     </b-carousel>
 
-    <div class="bg-dark text-light">
-      <div>
-        <h2>{{slide}}</h2>
-      </div>
-    </div>
     <div>
       <h4>GET STARTED WITH OWL API</h4>
       <ul class="p-3">
@@ -91,9 +98,14 @@ export default {
 };
 </script>
 
-<style>
+<style (scoped)>
 .question_text {
   font-weight: bold;
   font-style: italic;
 }
+
+.caption_background {
+  background-color: rgb(0,0,0, .5);
+}
+
 </style>
